afffafbba0f9e60a15a545d1f0b5585b8bc6f46b
Douglas <douglasmsrodrigues@hotmail.com>
2025-07-07 23:53:13 -0300

    Estilizando a sidebar

--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,38 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer, ListaIcones } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/DouglassenG.png" />
-    <Titulo fontSize={20}>Douglas Michelini</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Douglas Michelini</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        DouglassenG
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+      <ListaIcones>
+        <li>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+        </li>
+        <li>
+          <a>
+            <img src="" alt="" />
+          </a>
+        </li>
+        <li>
+          <a>
+            <img src="" alt="" />
+          </a>
+        </li>
+      </ListaIcones>
+    </SidebarContainer>
   </aside>
 )
 
